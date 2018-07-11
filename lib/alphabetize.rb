d9f1ca6ef2bf8f4668c8ea1a24c75f9ce37d8f3d
Ronald <ronaldcuello11@gmail.com>
2018-07-10 21:44:24 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
-def alphabetize(arr)
-  # code here
+Esperanto_Alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(array)
+#the method alphabetize takes the arguemnt
+array.sort_by do |words|
+  #we will sort the array and name each element as word
+  words.split("").collect do |letter|
+# the words will be spilt using "" and will collect each of the letter
+
+Esperanto_Alphabet.index(letter)
+#sort by the index of Esperanto_Alphabet using argument(letter)
+end
+end
 end
